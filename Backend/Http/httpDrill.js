b8ef493ca9b4a16a69c60446fbdb85f21702cf37
nadhim <nadhimregent@gmail.com>
2025-03-06 15:04:45 +0530

    gracefully handle errors

--- Backend/Http/httpDrill.js
+++ Backend/Http/httpDrill.js
@@ -1,59 +1,53 @@
-const http = require("http")
-const fs = require("fs")
-const { v4: uuid } = require("uuid")
+const http  =  require("http")
+const fs  =  require("fs")
+const { v4 : uuid }  =  require("uuid")
 const PORT = 5000
 
 const firstServer = http.createServer((req, res) => {
-  if (req.method != "GET") {
-    res.end()
-  }
 
-  if (req.url == "/") {
-    res.end("Server Working")
-  }
-
-  if (req.url == "/html") {
-
-    res.setHeader("Content-type", "text/html")
-
-
-    fs.readFile("./index.html", "utf-8", (err, data) => {
-      if (err) {
-        res.end(err.message)
-      }
-
-      res.end(data)
-    })
-
-  }
-
-  if (req.url == "/json") {
-    res.setHeader("Content-type", "text/json")
-    fs.readFile("./jsonData.json", "utf-8", (err, data) => {
-      if (err) {
-        res.end(err.message)
-      }
-
-      res.end(data)
-    })
-
-  }
-
-  if (req.url == "/uuid") {
-    res.setHeader("Content-type", "text/plain")
-    res.end( JSON.stringify({  uuid : `${uuid()}`}) )
-  }
 
   const urlArry = req.url.split("/")
 
-  if (urlArry[1] == "status") {
+
+  if (req.method != "GET") {
+    res.writeHead(200  , {} )
+    res.end()
+  } else   if (req.url == "/") {
+    res.end("Server Working")
+  } else  if (req.url == "/html") {
+
+    fs.readFile("./index.html", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead( 500 , {"Content-type": "text/plain" } )
+        res.end(err.message)
+      }
+
+      res.writeHead( 200 , {"Content-type": "text/html" } )
+      res.end(data)
+    })
+
+  } else   if (req.url == "/json") {
+    res.setHeader("Content-type", "text/json")
+    fs.readFile("./jsonData.json", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead( 500 , {"Content-type": "text/plain" } )
+        res.end(err.message)
+      }
+
+      res.writeHead( 200 , {"Content-type": "text/json" } )
+      res.end(data)
+    })
+
+  } else   if (req.url == "/uuid") {
+    res.setHeader("Content-type", "text/plain")
+    res.end( JSON.stringify({  uuid : `${uuid()}`}) )
+  }   if (urlArry[1] == "status") {
+
     const responseCode = parseInt(urlArry[2])
-    res.writeHead(responseCode)
+    res.writeHead( responseCode , {"Content-type": "text/plain"} )
 
     res.end(`Got response with ${responseCode} code`)
-  }
-
-  if (urlArry[1] == "delay") {
+  } else   if (urlArry[1] == "delay") {
   
     const delayInSeconds = parseFloat(urlArry[2])
 
@@ -63,10 +57,11 @@ const firstServer = http.createServer((req, res) => {
     }
 
     setTimeout(doThisAfterDelay, delayInSeconds * 1000)
+  } 
 
-
-  }
-})
+} 
+ 
+)
 
 firstServer.listen(PORT, () => {
   console.log(`Server working on http://localhost:${PORT}`)
