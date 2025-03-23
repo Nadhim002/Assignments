a6947685f1c833bd0cb7c55239aeed4e0f6b5e46
nadhim <nadhimregent@gmail.com>
2025-03-24 00:16:17 +0530

    add button disable funtionality

--- React/mini-projects/transfer-list/src/components/ButtonHolder.jsx
+++ React/mini-projects/transfer-list/src/components/ButtonHolder.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
-export default function ButtonHolder( { leftCount , rightCount  , onClickHandlers } ) {
+
+export default function ButtonHolder( { leftCount , rightCount  , onClickHandlers , leftSelectedCount ,  rigtSelectedCount } ) {
 
   const buttonClassName = 'p-2 border border-black bg-gray-200'
 
+  const isAllLeftDisabled = leftCount == 0 
+  const isAllRighttDisabled = rightCount == 0 
+
+  const isLeftDisabled = leftSelectedCount == 0 
+  const isRighttDisabled = rigtSelectedCount == 0 
 
   return (
     <div className='flex flex-col justify-center'>
-        <button className = {buttonClassName + `${ (rightCount == 0 ) ?  "opacity-50" : "" }` } onClick=  { () => { onClickHandlers.toAllLeft() } } disabled = { rightCount == 0  } >&lt;&lt;</button>
-        <button className = {buttonClassName} onClick=  { () => { onClickHandlers.toLeft() } } >&lt;</button>
-        <button className = {buttonClassName} onClick=  { () => { onClickHandlers.toRight() } } >&gt;</button>
-        <button className = {buttonClassName  + `${ (leftCount == 0 ) ?  "opacity-50" : "" }` } onClick=  { () => { onClickHandlers.toAllRight() } }  disabled = { leftCount == 0  }  >&gt;&gt;</button>
+        <button className = {buttonClassName + `${ isAllRighttDisabled ?  "opacity-50" : "" }` } onClick=  { () => { onClickHandlers.toAllLeft() } } disabled = { isAllRighttDisabled  } >&lt;&lt;</button>
+        <button className = {buttonClassName + `${ isRighttDisabled ?  "opacity-50" : "" }` } onClick=  { () => { onClickHandlers.toLeft() } } disabled = { isRighttDisabled  }  >&lt;</button>
+        <button className = {buttonClassName + `${ isLeftDisabled ?  "opacity-50" : "" }` } onClick=  { () => { onClickHandlers.toRight() } } disabled = {  isLeftDisabled }  >&gt;</button>
+        <button className = {buttonClassName  + `${ isAllLeftDisabled ?  "opacity-50" : "" }` } onClick=  { () => { onClickHandlers.toAllRight() } }  disabled = { isAllLeftDisabled  }  >&gt;&gt;</button>
     </div>
   )
 }
--- React/mini-projects/transfer-list/src/components/TransferList.jsx
+++ React/mini-projects/transfer-list/src/components/TransferList.jsx
@@ -5,14 +5,12 @@ import ListHolder from "./ListHolder"
 import { leftLisData, rightLisData } from "../assets/data.js"
 
 export default function TransferList() {
-
   const [leftList, setLeftList] = useState(leftLisData)
   const [rightList, setRightList] = useState(rightLisData)
 
-  function changeChecked( isLeft, id ) {
-
+  function changeChecked(isLeft, id) {
     if (isLeft) {
-      const newStorage =leftList.map((each) => {
+      const newStorage = leftList.map((each) => {
         if (each.id == id) {
           return { id: id, content: each.content, isSelected: !each.isSelected }
         }
@@ -30,24 +28,77 @@ export default function TransferList() {
     }
   }
 
-  function toLeft(){
+  function toLeft() {
 
+    const toMove = []
+    const toRetain = []
+
+    rightList.forEach((each) => {
+      if (each.isSelected) {
+        toMove.push({
+          id: each.id,
+          content: each.content,
+          isSelected: !each.isSelected,
+        })
+      } else{
+        toRetain.push(each)
+      }
+
+    })
+
+    setRightList( toRetain )
+    setLeftList( [...leftList , ...toMove ] )
 
   }
 
-  function toAllLeft(){
-    setLeftList([...leftList , ...rightList ])
+  function toAllLeft() {
+    setLeftList([...leftList, ...rightList])
     setRightList([])
   }
 
-  function toRight() {}
+  function toRight(){
 
-  function toAllRight(){
-    setRightList([...rightList , ...leftList ])
+    const toMove = []
+    const toRetain = []
+
+    leftList.forEach((each) => {
+      if (each.isSelected) {
+        toMove.push({
+          id: each.id,
+          content: each.content,
+          isSelected: !each.isSelected,
+        })
+      } else{
+        toRetain.push(each)
+      }
+
+    })
+
+    setLeftList( toRetain )
+    setRightList( [...rightList , ...toMove ] )
+
+  }
+
+  function toAllRight() {
+    setRightList([...rightList, ...leftList])
     setLeftList([])
   }
 
-  const onClickHandlers = { toLeft , toAllLeft , toRight ,  toAllRight  }
+  function noOfValuesSelected( list ){
+
+    return list.reduce( ( count , curr ) => {
+
+        if( curr["isSelected"] ){
+            count += 1
+        }
+
+        return count
+
+    } , 0 )
+
+  }
+
+  const onClickHandlers = { toLeft, toAllLeft, toRight, toAllRight }
 
   return (
     <div className="flex ">
@@ -56,7 +107,13 @@ export default function TransferList() {
         changeChecked={changeChecked}
         isLeft={true}
       />
-      <ButtonHolder leftCount = { leftList.length } rightCount = { rightList.length } onClickHandlers = {onClickHandlers} />
+      <ButtonHolder
+        leftCount={leftList.length}
+        rightCount={rightList.length}
+        onClickHandlers={onClickHandlers}
+        leftSelectedCount = { noOfValuesSelected(leftList) }
+        rigtSelectedCount = { noOfValuesSelected(rightList) }
+      />
       <ListHolder
         listData={rightList}
         changeChecked={changeChecked}
